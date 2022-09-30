8a1d9ac7e3d3c0aa9ae9abb83311aa5596339248
Omkar <omkar.mahajan872@gmail.com>
2022-09-30 09:46:54 +0530

    page visible

--- tranfusions/models.py
+++ tranfusions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class tranfusion(models.Model):
+    employee_id = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    patient_id = models.IntegerField()
+    patient_name = models.TextField(max_length=50)
+    blood_product_type = models.TextField(max_length=20)
+    blood_product_id = models.IntegerField()
+    
--- tranfusions/views.py
+++ tranfusions/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def tranfusionsHome(request):
+    return render(request, 'tranfusion.html')
